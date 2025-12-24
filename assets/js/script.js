ea7f77c009a2d39a64c1109bb1752dc741d973b8
Tanveer Ahmed Taher <taherahmed6927@gmail.com>
2025-12-25 01:29:39 +0600

    dsa

--- assets/js/script.js
+++ assets/js/script.js
@@ -184,7 +184,7 @@ const swiper = new Swiper('.swiper', {
 const nav = document.getElementById("nav");
 
 window.addEventListener("scroll", () => {
-    if (window.scrollY > 300) {
+    if (window.scrollY > 100) {
         nav.classList.add("scrolled");
     } else {
         nav.classList.remove("scrolled");
